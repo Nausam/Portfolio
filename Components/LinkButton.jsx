e4f003a49223fcb8597beac25df87f2c5d26c6e8
Nausam <nausam.hussain@gmail.com>
2023-11-27 11:26:01 +0500

    Added button animations

--- Components/LinkButton.jsx
+++ Components/LinkButton.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const LinkButton = ({ text, href, target }) => {
   return (
-    <div className="relative group">
-      <div className="absolute inset-0 transition-all duration-300 bg-gradient-to-r from-fuchsia-500 via-purple-500 to-purple-500 bg-fuchsia-500 rounded-lg blur opacity-0"></div>
+    <div>
+      <div className="transition-all duration-300 bg-gradient-to-r from-fuchsia-500 via-purple-500 to-purple-500 bg-fuchsia-500 rounded-lg blur opacity-0"></div>
       <Link href={href} target={target} className="relative custom_btn_lg">
         {text}
       </Link>
